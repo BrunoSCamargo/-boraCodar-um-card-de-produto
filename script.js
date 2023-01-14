98ee259adac04bd6886e52667257a3bed60312de
Bruno <Bruno_camargoSC@hotmail.com>
2023-01-14 17:41:25 -0300

    adjust in code

---
+++ script.js
@@ -0,0 +1,26 @@
+const button = document.querySelector(".but");
+const secondbutton = document.querySelector(".butbut");
+const sofa2d = document.querySelector(".sofa2d");
+const sofagif = document.querySelector(".sofagif");
+const btn = document.querySelector(".btn");
+const text = document.querySelector(".text");
+
+button.addEventListener("click", () => {
+  button.classList.add("hide");
+  secondbutton.classList.remove("hide");
+
+  sofa2d.classList.add("hide");
+  sofagif.classList.remove("hide");
+});
+
+secondbutton.addEventListener("click", () => {
+  secondbutton.classList.add("hide");
+  button.classList.remove("hide");
+
+  sofa2d.classList.remove("hide");
+  sofagif.classList.add("hide");
+});
+
+btn.addEventListener("click", () => {
+  text.innerHTML = "Item adicionado aos favoritos";
+});
